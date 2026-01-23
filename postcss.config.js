b51ccdeb91d9987bd43c6366cb7e9cb60f31cf3a
Roy Eyal <roy.eyal@gmail.com>
2026-01-23 20:29:14 +0200

    Add postcss-nesting dependency and integrate into PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
+import postcssNesting from 'postcss-nesting';
 
 export default {
   plugins: [
+    postcssNesting,
     autoprefixer,
     cssnano({
       preset: 'default'
